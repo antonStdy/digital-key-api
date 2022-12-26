789dff75a2b7fc9d7223578f48773cfdf86cf827
antonclaro <anton@claro.hr>
2022-12-26 21:17:08 +0200

    init

---
+++ src/main/java/com/ispk/digitalkeyapi/DigitalKeyApiApplication.java
@@ -0,0 +1,13 @@
+package com.ispk.digitalkeyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalKeyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalKeyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ispk/digitalkeyapi/DigitalKeyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispk.digitalkeyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalKeyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
